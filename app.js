271b7eb28ffd8ef8ad9980317f11a02342339c5c
Максим Маслов <flyover31@yandex.ru>
2020-02-02 00:44:42 +0300

    начал 15 спринт

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-require('dotenv').config();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
+const { PORT } = require('./secret');
 
-const { PORT = 3000 } = process.env;
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 const auth = require('./middlewares/auth');
@@ -19,6 +21,41 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+
+app.use(cookieParser());
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Загрузка сервера  пре');
+  }, 0);
+});
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2),
+    avatar: Joi.string().required(),
+  }),
+}), createUser);
+
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2),
+    avatar: Joi.string().required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+
+app.use(errorLogger);
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,5 +67,14 @@ app.use(auth);
 app.use('/', users);
 app.use('/', cards);
 
-app.listen(PORT, () => {});
+app.use(errorLogger);
+app.use(errors());
+
 app.all('/*', (req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).send({ message: err.message });
+  next();
+});
+
+app.listen(PORT, () => {});
