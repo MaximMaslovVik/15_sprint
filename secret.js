7d1428925f1426ca5ba3686de9e7238862957f8c
Максим Маслов <flyover31@yandex.ru>
2020-03-15 10:48:45 +0300

    Add files via upload

--- secret.js
+++ secret.js
@@ -1,3 +1,4 @@
 module.exports.PORT = process.env.PORT || 3000;
 module.exports.JWT_SECRET = process.env.JWT_SECRET || 'secret key';
 module.exports.CODE = process.env.NODE_ENV !== 'production';
+require('dotenv').config();
